92d0a4bf07276a08eae8a660b145f98529fafb83
Alsania14 <alingotama14@gmail.com>
2021-03-07 21:14:07 +0800

    update

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return 'failure';
+            return Redirect()->Route('failure');
         }
     }
 }
