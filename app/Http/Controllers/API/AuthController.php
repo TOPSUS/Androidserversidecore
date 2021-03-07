cbf670a248f74d9d98b9d6af91e43dfd869d14ce
Alsania14 <alingotama14@gmail.com>
2021-03-07 20:37:44 +0800

    update

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -19,6 +19,7 @@ class AuthController extends Controller
     public function login(Request $request){
         if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
             $user = Auth::user();
+            
             $token =  $user->createToken('nApp')->accessToken;
 
             return response()->json([
--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login','API\AuthController@login');
 Route::post('/register','API\AuthController@register');
-Route::get('/userprofile','API\UserController@detail');
+
 
 Route::group(['middleware' => 'auth:api'],function(){
+    Route::post('/userprofile','API\UserController@detail');
 });
