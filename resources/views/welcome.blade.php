5a7c8f71657c2ccda3690331247755b552589b83
Alsania14 <alingotama14@gmail.com>
2021-03-11 05:09:17 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    {{ public_path() }}
+                    VERIFIKASI SUKSES
                 </div>
             </div>
         </div>
