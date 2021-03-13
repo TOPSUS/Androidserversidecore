aef0d46f46e07f1d5af4f04be492cd8f1c0cd82b
Alsania14 <alingotama14@gmail.com>
2021-03-13 22:43:55 +0800

    Update Berita

---
+++ app/Http/Controllers/API/BeritaEspeedController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\BeritaEspeed;
+
+class BeritaEspeedController extends Controller
+{
+    public function getAllBeritaEpseed(){
+        $beritas = BeritaEspeed::all();
+
+        $response_beritas = [];
+
+        foreach ($beritas as $index => $berita) 
+        {
+            $response_beritas[] = [
+                'id' => $berita->id,
+                'id_speedboat' => $berita->id_speedboat,
+                'id_user' => $berita->id_user,
+                'judul' => $berita->judul,
+                'berita' => $berita->berita,
+                'tanggal' => $berita->tanggal,
+                'foto' => $berita->foto,
+            ]; 
+        }
+
+        return response()->json([
+            'response_code' => '200',
+            'status' => 'success',
+            'error' => (Object)[],
+            'berita_pelabuhan' => $response_beritas
+        ]);
+        
+    }
+}
