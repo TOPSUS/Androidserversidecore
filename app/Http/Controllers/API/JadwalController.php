4d0b30bebc59f6fc6d5d93e2bf6acdf5090fdbb7
Alsania14 <alingotama14@gmail.com>
2021-03-15 22:36:04 +0800

    Update JadwalController.php

--- app/Http/Controllers/API/JadwalController.php
+++ app/Http/Controllers/API/JadwalController.php
@@ -11,8 +11,8 @@ class JadwalController extends Controller
 {
     public function getJadwal(Request $request){
         $jadwals = Jadwal::whereDate('waktu_berangkat','2021-03-16')
-                            ->where('id_asal_pelabuhan',$request->id_asal_pemesanan)
-                            ->where('id_tujuan_pelabuhan',$request->id_tujuan_pelabuhan)
+                            ->where('id_asal_pelabuhan',3)
+                            ->where('id_tujuan_pelabuhan',4)
                             ->get();
         
         return $jadwals;
