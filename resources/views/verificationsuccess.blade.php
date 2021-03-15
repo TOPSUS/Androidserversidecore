9354463cc594ef19d8be6cb8f689eb240613ce10
Alsania14 <alingotama14@gmail.com>
2021-03-16 06:33:32 +0800

    tambah halaman verifikasi sukses

---
+++ resources/views/verificationsuccess.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<Style>
+.container-grid{
+    position: relative;
+    display: grid;
+    height: 100vw;
+    width: 100%;
+    grid-template-areas: 
+        "header"
+        "section"
+    ;
+    grid-template-rows: 50% 50%;
+    transition: .7s;
+}
+
+header {
+    grid-area: header;
+    padding-top: 10px;
+    display: flex;
+    height: 100%;
+    flex-direction: column;
+    text-align: center;
+    gap:12px;
+    font-size: 12px;
+    
+}
+
+</Style>
+<body>
+    <div class="container-grid">
+        <header>
+            <img src="{{ asset('appimage/wing.png') }}" alt="">
+        </header>
+    </div>
+</body>
+</html>
