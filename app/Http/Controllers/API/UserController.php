16df9e6333263c080b139087d7b65f758c69ae77
Abdi Purnawan <abdipurnawan1@gmail.com>
2021-03-15 15:46:06 +0800

    test edit lagiiii

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -55,11 +55,12 @@ class UserController extends Controller
 
         if($request->photo!=''){
             $photo = time().'.jpg';
-            file_put_contents($photo,base64_decode($request->photo));
+            Storage::putFile('public_html/image_users',base64_decode($request->photo));
+            //file_put_contents($photo,base64_decode($request->photo));
             // $simpan_image_profile = Storage::putFile('public_html/image_users',base64_decode($request->photo));
             // $simpan_image_profile = basename($simpan_image_profile);
             // //file_put_contents('public_html/image_users'.$photo,base64_decode($request->photo));
-            // //Storage::putFile('public_html/image_users',base64_decode($request->photo));
+            Storage::putFile('public_html/image_users/$photo',base64_decode($request->photo));
             // $user->foto = $simpan_image_profile;
             // File::put(public_path().'/project_images/'.$imageName, base64_decode($request->encoded_image));
         }
