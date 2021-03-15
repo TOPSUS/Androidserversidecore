d276c201fd67eace219b8faaf65b139523af877c
Alsania14 <alingotama14@gmail.com>
2021-03-15 20:14:38 +0800

    Menambahkan JadwalController

---
+++ app/Http/Controllers/API/JadwalController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Jadwal;
+
+class JadwalController extends Controller
+{
+    public function getJadwal(Request $request){
+        return 'hai';
+    }
+}
