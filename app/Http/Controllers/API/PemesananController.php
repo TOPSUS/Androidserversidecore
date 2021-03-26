7d4649990f94b43940d0c21f58e7ee3279aac2b2
Alsania14 <alingotama14@gmail.com>
2021-03-27 02:38:06 +0800

    Update PemesananController.php

--- app/Http/Controllers/API/PemesananController.php
+++ app/Http/Controllers/API/PemesananController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 use App\Pembelian;
+use App\Jadwal;
 
 class PemesananController extends Controller
 {
@@ -23,7 +24,7 @@ class PemesananController extends Controller
             ],[
                 'id_pemesan' => 'required',
                 'id_jadwal' => 'required',
-                'penumpang' => 'array'
+                'penumpang' => 'required|array'
             ]);
 
             if($validator->fails()){
@@ -36,13 +37,38 @@ class PemesananController extends Controller
             }
         // AKHIR
 
-        // MAIN LOGIC BUAT SEBUAH PEMESANAN
-            return response()->json([
-                'response_code' => 200,
-                'status' => 'success',
-                'message' => 'berhasil create pemesanan',
-                'error' => (Object)[],
-            ],200);
+        // MAIN LOGIC BUAT SEBUAH PEMESANAN                        
+            // CEK APAKAN JADWAL MASIH TERSEDIA UNTUK SEMUA PENUMPANG
+                $jadwal = Jadwal::find($request->id_jadwal);
+                
+                if($jadwal == null){
+                    return response()->json([
+                        'response_code' => 401,
+                        'status' => 'failure',
+                        'message' => 'Tidak ditemukan id_jadwal yang dimaksud',
+                        'error' => (Object)[],
+                    ],200);
+                }
+                
+                $speedboat = $jadwal->getBoat();
+                $total_pembelian_saat_ini = $jadwal->getTotalPembelianSaatini();
+                
+                if(($speedboat->kapasitas - $total_pembelian_saat_ini) >= count($penumpang_decode)){
+                    return response()->json([
+                        'response_code' => 200,
+                        'status' => 'success',
+                        'message' => 'berhasil create pemesanan',
+                        'error' => (Object)[],
+                    ],200);    
+                }else{
+                    return response()->json([
+                        'response_code' => 402,
+                        'status' => 'failure',
+                        'message' => 'gagal jumlah ticket tersisa kurang',
+                        'error' => (Object)[],
+                    ],200);
+    
+                }
         // AKHIR
     }
 }
