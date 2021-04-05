de4f6af0c5778f9ffb66ffcd01b4e38ee3be7f36
Alsania14 <alingotama14@gmail.com>
2021-04-05 15:10:23 +0800

    Update JadwalController.php

--- app/Http/Controllers/API/JadwalController.php
+++ app/Http/Controllers/API/JadwalController.php
@@ -61,7 +61,7 @@ class JadwalController extends Controller
 
                 // BUAT WAKTU SAMPAI DENGAN CARBON;
                 $string_waktu_berangkat = $jadwal->tanggal." ".$jadwal->waktu_berangkat;
-                return $string_waktu_berangkat;
+                return $jadwal->tanggal;
                 $jadwals[$index]->wakktu_sampai = Carbon::createFromFormat("y-m-d H:i:s",$string_waktu_berangkat);
             }
         }else{
