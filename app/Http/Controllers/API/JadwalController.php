c2fd7a7656a7e7167705c0ddac6e36f38b129da1
Alsania14 <alingotama14@gmail.com>
2021-04-05 15:04:43 +0800

    test

--- app/Http/Controllers/API/JadwalController.php
+++ app/Http/Controllers/API/JadwalController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
-use Carbon\Carbon;
+use Carbon;
 use App\Jadwal;
 
 class JadwalController extends Controller
