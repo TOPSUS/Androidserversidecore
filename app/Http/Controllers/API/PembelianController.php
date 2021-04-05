89b3d3f9c9573c32cb2aa72183acc101147ef88f
Abdi Purnawan <abdipurnawan1@gmail.com>
2021-04-05 19:01:11 +0800

    detail pembelian

--- app/Http/Controllers/API/PembelianController.php
+++ app/Http/Controllers/API/PembelianController.php
@@ -89,7 +89,7 @@ class PembelianController extends Controller
         //GET JADWAL PEMBELIAN, PELABUHAN, WAKTU, KAPAL
         $jadwal = $pembelian->getJadwal();
 
-        $speedboat = $jadwal->getBoat()->nama_speedboat;
+        $speedboat = $jadwal->getKapal()->first()->nama_kapal;
         $tanggal = $jadwal->tanggal;
         $harga = $pembelian->total_harga;
         $pelabuhan_asal = $jadwal->getPelabuhanAsal()->nama_pelabuhan;
