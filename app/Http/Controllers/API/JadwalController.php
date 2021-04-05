4765662efc7368cc0d123a91cbaab768aca19ce7
Alsania14 <alingotama14@gmail.com>
2021-04-05 15:08:59 +0800

    Update JadwalController.php

--- app/Http/Controllers/API/JadwalController.php
+++ app/Http/Controllers/API/JadwalController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
-use Carbon;
+use Carbon\Carbon;
 use App\Jadwal;
 
 class JadwalController extends Controller
