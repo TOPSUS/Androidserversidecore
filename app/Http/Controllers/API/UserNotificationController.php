ec226f3ef0ec2f5a7c54a93d9115e5378ad1bcf3
Alsania14 <alingotama14@gmail.com>
2021-04-14 12:36:34 +0800

    Update UserNotificationController.php

--- app/Http/Controllers/API/UserNotificationController.php
+++ app/Http/Controllers/API/UserNotificationController.php
@@ -7,9 +7,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 use App\User;
+use App\UserNotification;
+use Illuminate\Support\Facades\Http;
 
 class UserNotificationController extends Controller
 {
+    /**
+     * METHOD NOTIFIKASI KE DALAM USER
+     */
     public function addNotification(Request $request){
         
     }
