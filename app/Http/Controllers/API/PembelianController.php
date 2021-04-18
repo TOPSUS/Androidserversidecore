3115bf3c6db1143fe379b711bd8218473eed02ae
Alsania14 <alingotama14@gmail.com>
2021-04-18 11:35:22 +0800

    Update PembelianController.php

--- app/Http/Controllers/API/PembelianController.php
+++ app/Http/Controllers/API/PembelianController.php
@@ -19,24 +19,25 @@ use App\Http\Helper\NotificationHelper;
 
 class PembelianController extends Controller
 {
-    public function getPembelian(Request $request){
+    public function getPembelian(Request $request)
+    {
         $user = User::find(Auth::user()->id);
-        if($request->status == "menunggu pembayaran"){
-            $pembelians = Pembelian::where('id_user', $user->id)->where(function($q) {
+        if ($request->status == "menunggu pembayaran") {
+            $pembelians = Pembelian::where('id_user', $user->id)->where(function ($q) {
                 $q->where('status', 'menunggu pembayaran')
-                  ->orWhere('status', 'menunggu konfirmasi');
+                    ->orWhere('status', 'menunggu konfirmasi');
             })->orderBy('id', 'DESC')->get(
                 ['id', 'id_jadwal', 'id_user', 'tanggal', 'total_harga', 'status']
             );
-        }else if($request->status == "terkonfirmasi"){
+        } else if ($request->status == "terkonfirmasi") {
             $pembelians = Pembelian::where('id_user', $user->id)->where('status', $request->status)->orderBy('id', 'DESC')->get(
                 ['id', 'id_jadwal', 'id_user', 'tanggal', 'total_harga', 'status']
             );
-        }else if($request->status == "done"){
-            $pembelians = Pembelian::where('id_user', $user->id)->where(function($q) {
+        } else if ($request->status == "done") {
+            $pembelians = Pembelian::where('id_user', $user->id)->where(function ($q) {
                 $q->where('status', 'digunakan')
-                  ->orWhere('status', 'dibatalkan')
-                  ->orWhere('status', 'expired');
+                    ->orWhere('status', 'dibatalkan')
+                    ->orWhere('status', 'expired');
             })->orderBy('id', 'DESC')->get(
                 ['id', 'id_jadwal', 'id_user', 'tanggal', 'total_harga', 'status']
             );
@@ -56,31 +57,32 @@ class PembelianController extends Controller
 
             $pembelians[$index]->tanggal = $jadwal->tanggal;
             $pembelians[$index]->waktu_berangkat = $waktu_asal;
-            
-            $pembelians[$index]->waktu_sampai = Carbon::createFromFormat("H:i:s",$pembelian->waktu_berangkat)
-                                                ->addMinutes($jadwal->estimasi_waktu)->format("H:i:s");
+
+            $pembelians[$index]->waktu_sampai = Carbon::createFromFormat("H:i:s", $pembelian->waktu_berangkat)
+                ->addMinutes($jadwal->estimasi_waktu)->format("H:i:s");
         }
 
-        if($pembelians != null){
+        if ($pembelians != null) {
             return response()->json([
                 'response_code' => 200,
                 'status' => 'success',
                 'message' => 'berhasil mendapatkan pembelian',
-                'error' => (Object)[],
+                'error' => (object)[],
                 'pembelian' => $pembelians
-            ],200);
-        }else{
+            ], 200);
+        } else {
             return response()->json([
                 'response_code' => 401,
                 'status' => 'success',
                 'message' => 'gagal terjadi kesalahan',
-                'error' => (Object)[],
+                'error' => (object)[],
                 'pembelian' => []
-            ],200);
+            ], 200);
         }
     }
 
-    public function getdetailpembelian(Request $request){
+    public function getdetailpembelian(Request $request)
+    {
         //GET USER
         $user = User::find(Auth::user()->id);
 
@@ -88,13 +90,13 @@ class PembelianController extends Controller
         $pembelian = Pembelian::where('id', $request->id)->first();
         //getSisaWaktu
         $sisaWaktu = $pembelian->created_at->format('Y-m-d H:i:s');
-        $sisaWaktu = Carbon::createFromFormat('Y-m-d H:i:s',$sisaWaktu)
-        ->addMinutes(60)->format('Y-m-d H:i:s');
+        $sisaWaktu = Carbon::createFromFormat('Y-m-d H:i:s', $sisaWaktu)
+            ->addMinutes(60)->format('Y-m-d H:i:s');
         $sisaWaktu = Carbon::now()->diffInMilliseconds($sisaWaktu, false);
-        if($pembelian->status != "menunggu pembayaran"){
+        if ($pembelian->status != "menunggu pembayaran") {
             $sisaWaktu = 0;
         }
-        if((int)$sisaWaktu < 60000 && $pembelian->status == "menunggu pembayaran")  {
+        if ((int)$sisaWaktu < 60000 && $pembelian->status == "menunggu pembayaran") {
             $pembelian->status = "expired";
             $pembelian->save();
             $pembelian = Pembelian::where('id', $request->id)->first();
@@ -109,13 +111,12 @@ class PembelianController extends Controller
         $pelabuhan_asal = $jadwal->getPelabuhanAsal()->nama_pelabuhan;
         $pelabuhan_tujuan = $jadwal->getPelabuhanTujuan()->nama_pelabuhan;
         $waktu_berangkat = $jadwal->waktu_berangkat;
-        $waktu_sampai = Carbon::createFromFormat("H:i:s",$waktu_berangkat)
-                        ->addMinutes($jadwal->estimasi_waktu)->format("H:i:s");
+        $waktu_sampai = Carbon::createFromFormat("H:i:s", $waktu_berangkat)
+            ->addMinutes($jadwal->estimasi_waktu)->format("H:i:s");
         $pembayaran = $pembelian->getPembayaran();
         $metode_pembayaran = $pembayaran->nama_metode;
         $rekening = $pembayaran->nomor_rekening;
         $logo_metode = $pembayaran->logo_metode;
-
 
 
         $status_transaksi = $pembelian->status;
@@ -137,12 +138,12 @@ class PembelianController extends Controller
             $penumpangs[$index]->no_id_card = $card;
         }
 
-        if($pembelian != null){
+        if ($pembelian != null) {
             return response()->json([
                 'response_code' => 200,
                 'status' => 'success',
                 'message' => 'berhasil mendapatkan pembelian',
-                'error' => (Object)[],
+                'error' => (object)[],
                 'kapal' => $speedboat,
                 'tanggal' => $tanggal,
                 'harga' => $harga,
@@ -151,7 +152,7 @@ class PembelianController extends Controller
                 'waktu_berangkat' => $waktu_berangkat,
                 'waktu_sampai' => $waktu_sampai,
                 'status_transaksi' => $status_transaksi,
-                'sisa_waktu' =>$sisa_waktu,
+                'sisa_waktu' => $sisa_waktu,
                 'nama_pemesan' => $nama_pemesan,
                 'email_pemesan' => $email_pemesan,
                 'telepon_pemesan' => $telepon_pemesan,
@@ -161,159 +162,168 @@ class PembelianController extends Controller
                 'rekening' => $rekening,
                 'logo_metode' => $logo_metode,
                 'penumpang' => $penumpangs
-            ],200);
-        }else{
+            ], 200);
+        } else {
             return response()->json([
                 'response_code' => 401,
                 'status' => 'success',
                 'message' => 'gagal terjadi kesalahan',
-                'error' => (Object)[],
+                'error' => (object)[],
                 'pembelian' => []
-            ],200);
+            ], 200);
         }
     }
 
     // MENGUPLOAD BUKTI PEMBELIAN TICKET
-    public function uploadButkiPembelian(Request $request){
+    public function uploadButkiPembelian(Request $request)
+    {
         // LARAVEL VALIDATOR
-            $validator = Validator::make($request->all(),[
-                'id_pembelian' => 'required|numeric',
-                'image_bukti_pembayaran' => 'required|image|max:1000'
-            ]);
+        $validator = Validator::make($request->all(), [
+            'id_pembelian' => 'required|numeric',
+            'image_bukti_pembayaran' => 'required|image|max:1000'
+        ]);
 
-            if($validator->fails()){
-                return response()->json([
-                    'response_code' => 402,
-                    'status' => 'failure',
-                    'message' => 'terdapat format yang salah',
-                    'error' => $validator->errors(),
-                ],200);
-            }
+        if ($validator->fails()) {
+            return response()->json([
+                'response_code' => 402,
+                'status' => 'failure',
+                'message' => 'terdapat format yang salah',
+                'error' => $validator->errors(),
+            ], 200);
+        }
         // AKHIR
 
         // MAIN PROCESS UPDATE PEMBELIAN TABLE DAN SIMPAN BUKTI PEMBAYARAN
-            // CARI RECORD PEMBELIAN DENGAN ID
-                $pembelian = Pembelian::find($request->id_pembelian);
-            
-                // APABILA KOSONG PEMBELIANNYA MAKA AKAN DIRETURN HASIL BERIKUT
-                if($pembelian == null){
-                    return response()->json([
-                        'response_code' => 402,
-                        'status' => 'failure',
-                        'message' => 'id yang dimaksud tidak ditemukan',
-                        'error' => (Object)[],
-                    ],200);
-                }
+        // CARI RECORD PEMBELIAN DENGAN ID
+        $pembelian = Pembelian::find($request->id_pembelian);
 
-                // CEK APAKAH BENAR TRANSAKSI INI MILIK USER YANG SEDANG LOGIN
-                $user = Auth::user();
-
-                if($pembelian->id_user != $user->id){
-                    return response()->json([
-                        'response_code' => 403,
-                        'status' => 'failure',
-                        'message' => 'id user yang memanggil transaksi berbeda',
-                        'error' => (Object)[],
-                    ],200);
-                }
-                
-                // HAPUS FILE YANG SAMA DARI PEMBELIAN INI APABILA ADA DAN SIMPAN FILE BUKTI BARU
-                Storage::delete('public_html/bukti_pembayaran/'.($pembelian->bukti == null ? " " : $pembelian->bukti));
-                $bukti_pembayaran = Storage::putFile('public_html/bukti_pembayaran',$request->file('image_bukti_pembayaran'));
-                $bukti_pembayaran = basename($bukti_pembayaran);
-
-                // SIMAPN PEMBELIAN
-                $pembelian->status = 'menunggu konfirmasi';
-                $pembelian->bukti = $bukti_pembayaran;
-                $pembelian->update();
-
-            // AKHIR
-
-            // BERIKAN NOTIFIKASI BERHASIL UPLOAD BUKTI PEMBAYARAN KE USER
-            NotificationHelper::createNotification($user->id,$user->fcm_token,"Upload Bukti Berhasil","Upload bukti pembayaran dengan id ".$pembelian->id." telah berhasil dilakukan, mohon menunggu hingga pembayaran tervalidasi",
-            NotificationHelper::STATUS_DELIVERED,NotificationHelper::TYPE_SUKSES,NotificationHelper::NOTIFICATION_BY_SYSTEM);
-            
-            // RETURN BERHASIL MENYIMPAN BUKTI PEMBAYARAN
+        // APABILA KOSONG PEMBELIANNYA MAKA AKAN DIRETURN HASIL BERIKUT
+        if ($pembelian == null) {
             return response()->json([
-                'response_code' => 200,
-                'status' => 'success',
-                'message' => 'berhasil menyimpan file pembayaran',
-                'error' => (Object)[],
-            ],200);
+                'response_code' => 402,
+                'status' => 'failure',
+                'message' => 'id yang dimaksud tidak ditemukan',
+                'error' => (object)[],
+            ], 200);
+        }
+
+        // CEK APAKAH BENAR TRANSAKSI INI MILIK USER YANG SEDANG LOGIN
+        $user = Auth::user();
+
+        if ($pembelian->id_user != $user->id) {
+            return response()->json([
+                'response_code' => 403,
+                'status' => 'failure',
+                'message' => 'id user yang memanggil transaksi berbeda',
+                'error' => (object)[],
+            ], 200);
+        }
+
+        // HAPUS FILE YANG SAMA DARI PEMBELIAN INI APABILA ADA DAN SIMPAN FILE BUKTI BARU
+        Storage::delete('public_html/bukti_pembayaran/' . ($pembelian->bukti == null ? " " : $pembelian->bukti));
+        $bukti_pembayaran = Storage::putFile('public_html/bukti_pembayaran', $request->file('image_bukti_pembayaran'));
+        $bukti_pembayaran = basename($bukti_pembayaran);
+
+        // SIMAPN PEMBELIAN
+        $pembelian->status = 'menunggu konfirmasi';
+        $pembelian->bukti = $bukti_pembayaran;
+        $pembelian->update();
+
+        // AKHIR
+
+        // BERIKAN NOTIFIKASI BERHASIL UPLOAD BUKTI PEMBAYARAN KE USER
+        NotificationHelper::createNotification($user->id, $user->fcm_token, "Upload Bukti Berhasil", "Upload bukti pembayaran dengan id " . $pembelian->id . " telah berhasil dilakukan, mohon menunggu hingga pembayaran tervalidasi",
+            NotificationHelper::STATUS_DELIVERED, NotificationHelper::TYPE_SUKSES, NotificationHelper::NOTIFICATION_BY_SYSTEM);
+
+        // RETURN BERHASIL MENYIMPAN BUKTI PEMBAYARAN
+        return response()->json([
+            'response_code' => 200,
+            'status' => 'success',
+            'message' => 'berhasil menyimpan file pembayaran',
+            'error' => (object)[],
+        ], 200);
 
         // AKHIR
 
     }
 
     // BATALKAN PEMBELIAN TICKET ATAU JADWAL
-    public function batalkanPembelian(Request $request){
+    public function batalkanPembelian(Request $request)
+    {
         // LARAVEL VALIDATOR
-            $validator = Validator::make($request->all(),[
-                'id_pembelian' => 'required|numeric'
-            ]);
+        $validator = Validator::make($request->all(), [
+            'id_pembelian' => 'required|numeric'
+        ]);
 
-            if($validator->fails()){
-                return response()->json([
-                    'response_code' => 402,
-                    'status' => 'failure',
-                    'message' => 'terdapat format yang salah',
-                    'error' => $validator->errors(),
-                ],200);
-            }
+        if ($validator->fails()) {
+            return response()->json([
+                'response_code' => 402,
+                'status' => 'failure',
+                'message' => 'terdapat format yang salah',
+                'error' => $validator->errors(),
+            ], 200);
+        }
         // AKHIR
 
         // MAIN LOGIC
-            // MENCARI PEMBELIAN DENGAN ID YANG DIMAKSUD
-                $pembelian = Pembelian::find($request->id_pembelian);
+        // MENCARI PEMBELIAN DENGAN ID YANG DIMAKSUD
+        $pembelian = Pembelian::find($request->id_pembelian);
 
-                if($pembelian == null){
-                    return response()->json([
-                        'response_code' => 402,
-                        'status' => 'failure',
-                        'message' => 'id pembelian tidak ditemukan',
-                        'error' => (Object)[],
-                    ],200);
-                }
-            // AKHIR
-
-            // CHECK APAKAH STATUSNYA BELUM "DIBATALKAN"
-                if($pembelian->status == 'dibatalkan'){
-                    return response()->json([
-                        'response_code' => 200,
-                        'status' => 'success',
-                        'message' => 'pembelian sudah dibatalkan sebelumnya',
-                        'error' => (Object)[],
-                    ],200);
-                }
-
-            // MENGUBAH STATUS MENJADI DIBATALKAN PEMBELIAN
-                $pembelian->status = 'dibatalkan';
-                $pembelian->update();
-            // AKHIR
-            
-            // RETURN SUKSES RESPONSE
-                return response()->json([
-                    'response_code' => 200,
-                    'status' => 'success',
-                    'message' => 'pembelian berhasil dibatalkan',
-                    'error' => (Object)[],
-                ],200);
-            // AKHIR
-
-        // AKHIR
-    }
-
-    public function setpembelianstatus(Request $request){
-
-        $pembelian = Pembelian::where('id', $request->id)->first();
-
-        if($pembelian == null){
+        if ($pembelian == null) {
             return response()->json([
                 'response_code' => 402,
                 'status' => 'failure',
                 'message' => 'id pembelian tidak ditemukan',
-                'error' => (Object)[],
-            ],200);
+                'error' => (object)[],
+            ], 200);
+        }
+        // AKHIR
+
+        // GET USER
+        $user = Auth::user();
+
+        // BERIKAN NOTIFIKASI BERHASIL UPLOAD BUKTI PEMBAYARAN KE USER
+        NotificationHelper::createNotification($user->id, $user->fcm_token, "Transaksi dibatalkan", "Transaksi dengan id " . $id_pembelian . " telah dibatalkan.",NotificationHelper::STATUS_DELIVERED, NotificationHelper::TYPE_DANGER, NotificationHelper::NOTIFICATION_BY_SYSTEM);
+
+        // CHECK APAKAH STATUSNYA BELUM "DIBATALKAN"
+        if ($pembelian->status == 'dibatalkan') {
+            return response()->json([
+                'response_code' => 200,
+                'status' => 'success',
+                'message' => 'pembelian sudah dibatalkan sebelumnya',
+                'error' => (object)[],
+            ], 200);
+        }
+
+        // MENGUBAH STATUS MENJADI DIBATALKAN PEMBELIAN
+        $pembelian->status = 'dibatalkan';
+        $pembelian->update();
+        // AKHIR
+
+        // RETURN SUKSES RESPONSE
+        return response()->json([
+            'response_code' => 200,
+            'status' => 'success',
+            'message' => 'pembelian berhasil dibatalkan',
+            'error' => (object)[],
+        ], 200);
+        // AKHIR
+
+        // AKHIR
+    }
+
+    public function setpembelianstatus(Request $request)
+    {
+
+        $pembelian = Pembelian::where('id', $request->id)->first();
+
+        if ($pembelian == null) {
+            return response()->json([
+                'response_code' => 402,
+                'status' => 'failure',
+                'message' => 'id pembelian tidak ditemukan',
+                'error' => (object)[],
+            ], 200);
         }
 
         $pembelian->status = $request->status;
@@ -323,7 +333,7 @@ class PembelianController extends Controller
             'response_code' => 200,
             'status' => 'success',
             'message' => 'pembelian status berhasil diubah',
-            'error' => (Object)[],
-        ],200);
+            'error' => (object)[],
+        ], 200);
     }
 }
