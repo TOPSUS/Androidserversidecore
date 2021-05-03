60cbafeb3f509e23427b1e34dd18bc6e1532b015
Alsania14 <alingotama14@gmail.com>
2021-05-04 00:36:56 +0800

    menambahkan soft delete pada jadwal

--- app/Jadwal.php
+++ app/Jadwal.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Jadwal extends Model
 {
+    use SoftDeletes;
     protected $table = 'tb_jadwal';
 
     public function getPelabuhanAsal(){
