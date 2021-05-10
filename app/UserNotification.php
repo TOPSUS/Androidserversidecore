bb44bdb703e6bd8810ef93b12411344b752c6652
Alsania14 <alingotama14@gmail.com>
2021-05-10 22:37:43 +0800

    perbaikan datetime

--- app/UserNotification.php
+++ app/UserNotification.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserNotification extends Model
 {
+    use dateTimeFormater;
+
     protected $table = "tb_user_notification";
 
     // ATRIBUTE YANG DAPAT DI FILLABLE
@@ -19,6 +21,14 @@ class UserNotification extends Model
      * @var array
      */
     protected $casts = [
-        'created_at' => 'String',
+        'created_at' => 'datetime',
     ];
+}
+
+trait dateTimeFormater
+{
+    protected function serializeDate(\DateTimeInterface $date)
+    {
+        return $date->format("Y-m-d H:i:s");
+    }
 }
