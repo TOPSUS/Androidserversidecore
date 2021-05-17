cdffcc7e30737bb7125e93b97cdd6d3cf8e66918
Alsania14 <alingotama14@gmail.com>
2021-05-17 23:13:16 +0800

    Update Pembelian.php

--- app/Pembelian.php
+++ app/Pembelian.php
@@ -9,30 +9,30 @@ class Pembelian extends Model
     protected $table = 'tb_pembelian';
 
     public function getJadwal(){
-        return $this->hasOne('App\Jadwal','id','id_jadwal')->first();
+        return $this->hasOne('App\Jadwal','id','id_jadwal')->withTrashed()->first();
     }
 
     public function getPelabuhanAsal(){
-        return $this->hasOne('App\Pelabuhan','id','id_asal_pelabuhan')->first();
+        return $this->hasOne('App\Pelabuhan','id','id_asal_pelabuhan')->withTrashed()->first();
     }
 
     public function getPelabuhanTujuan(){
-        return $this->hasOne('App\Pelabuhan','id','id_tujuan_pelabuhan')->first();
+        return $this->hasOne('App\Pelabuhan','id','id_tujuan_pelabuhan')->withTrashed()->first();
     }
 
     public function getBoat(){
-        return $this->hasOne('App\SpeedBoat','id','id_speedboat')->first();
+        return $this->hasOne('App\SpeedBoat','id','id_speedboat')->withTrashed()->first();
     }
 
     public function getPembayaran(){
-        return $this->hasOne('App\MetodePembayaran','id','id_metode_pembayaran')->first();
+        return $this->hasOne('App\MetodePembayaran','id','id_metode_pembayaran')->withTrashed()->first();
     }
 
     public function getGolongan(){
-        return $this->hasOne('App\Golongan','id','id_golongan')->first();
+        return $this->hasOne('App\Golongan','id','id_golongan')->withTrashed()->first();
     }
 
     public function getReview(){
-        return $this->hasOne('App\Review','id_pembelian','id')->first();
+        return $this->hasOne('App\Review','id_pembelian','id')->withTrashed()->first();
     }
 }
