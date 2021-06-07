05e71b574329c1ba885bc6c458be1e5a10076733
Alsania14 <alingotama14@gmail.com>
2021-06-08 01:26:41 +0800

    Update PembelianController.php

--- app/Http/Controllers/API/PembelianController.php
+++ app/Http/Controllers/API/PembelianController.php
@@ -50,7 +50,7 @@ class PembelianController extends Controller
 
 
         foreach ($pembelians as $index => $pembelian) {
-            $jadwal = $pembelian->getDetailJadwal()->withTrashed()->first()->getJadwal()->first();
+            $jadwal = $pembelian->getDetailJadwal()->withTrashed()->first()->getJadwal();
             $pelabuhan_asal = $jadwal->getPelabuhanAsal();
             $pelabuhan_tujuan = $jadwal->getPelabuhanTujuan();
             $speedboat = $jadwal->getKapal()->withTrashed()->first();
@@ -114,7 +114,7 @@ class PembelianController extends Controller
         }
 
         //GET JADWAL PEMBELIAN, PELABUHAN, WAKTU, KAPAL
-        $jadwal = $pembelian->getDetailJadwal()->withTrashed()->first()->getJadwal()->first();
+        $jadwal = $pembelian->getDetailJadwal()->withTrashed()->first()->getJadwal();
         $speedboat = $jadwal->getKapal()->withTrashed()->first()->nama_kapal;
         $tanggal = $pembelian->tanggal;
         $harga = $pembelian->total_harga;
