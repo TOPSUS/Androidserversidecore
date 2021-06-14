b355e30e9d01dc41224a21fdebc47f293a73bec5
Alsania14 <alingotama14@gmail.com>
2021-06-14 21:09:28 +0800

    Update PemesananController.php

--- app/Http/Controllers/API/PemesananController.php
+++ app/Http/Controllers/API/PemesananController.php
@@ -168,8 +168,6 @@ class PemesananController extends Controller
                             $detail_pembelian->nama_pemegang_tiket = $penumpang->nama_pemegang_ticket;
                             $detail_pembelian->no_id_card = $penumpang->no_id_card;
 
-                            return "masuk";
-
                             // APABILA TIPE DARI JASA YANG DIMINTA ADALAH KENDARAAN MAKA PENUMPANG PERTAMA HARGA NYA SESUAI DENGAN GOLONGAN
                             if($request->tipe_kapal == 'feri' && $request->id_golongan != null){
                                 $detail_pembelian->harga = 0;
@@ -179,6 +177,8 @@ class PemesananController extends Controller
 
                             $detail_pembelian->status = "Not Used";
                             $detail_pembelian->save();
+
+                            return "masuk";
                         }
                     // AKHIR
 
